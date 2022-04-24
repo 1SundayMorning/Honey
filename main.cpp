559a05f522dbaf4a2d948869213cc37045d20013
Shaymus Alwan <shaymusalwan@Shaymuss-MacBook-Pro-2.local>
2022-04-24 16:51:53 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "SFML/Audio.hpp"
+#include <vector>
+
+#include "sound.h"
+
+using namespace std;
+
+int main() {
+    std::cout<<"Honey Test"<<std::endl;
+
+    sf::SoundBuffer buffer;
+    vector <sf::Int16> samples;
+
+    for (int i = 0; i < 44100; i++) {
+        // samples.push_back(sound::SquareWave(i, 440, 0.5));
+        // samples.push_back(sound::Noise(0.5)); 
+        samples.push_back(sound::SineWave(i, 440, 0.5));
+    }
+
+
+    buffer.loadFromSamples(&samples[0], samples.size(), 1, 44100);
+
+    sf::Sound sound;
+    sound.setBuffer(buffer);
+    sound.play();
+
+	while(1) {}
+
+    return 0;
+
+}
